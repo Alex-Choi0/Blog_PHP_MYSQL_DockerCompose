b130ce54eb96e436672414c7a8030cf8070975ad
alex <choijeaho86@gmail.com>
2023-12-04 04:36:46 +0900

    add comment

--- src/mysql.php
+++ src/mysql.php
@@ -1,8 +1,7 @@
 <!-- ./src/mysql.php -->
 <?php
-$env = parse_ini_file('../.env');
-$type = $env["PHP_TYPE"];
-$host = $env["MYSQL_HOST"];
+$env = parse_ini_file('../.env'); // env파일 경로를 확인해서 key, value로 저장
+$host = $env["MYSQL_HOST"]; // MySQL 연결 주소
 $username = $env["MYSQL_USERNAME"]; // MySQL 사용자 이름
 $password = $env["MYSQL_PASSWORD"]; // MySQL 사용자 비밀번호
 $dbname = $env["MYSQL_DATABASE"]; // 데이터베이스 이름
